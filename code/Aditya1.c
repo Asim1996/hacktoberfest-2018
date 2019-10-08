59828fa053e4de37ef46636c0e80e87e3c89d880
Zeeshan Alam <zalam52@gmail.com>
2019-10-08 20:44:10 +0530

    Implemented pointers
    implemented pointers wherever necessary

--- code/Aditya1.c
+++ code/Aditya1.c
@@ -24,16 +24,16 @@ int partition (int arr[], int low, int high)
     return (i + 1); 
 } 
 
-void quickSort(int arr[], int low, int high) 
+void quickSort(int *arr, int low, int high) 
 { 
     if (low < high) 
     {
         int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1); 
-        quickSort(arr, pi + 1, high); 
+        quickSort(&arr, low, pi - 1); 
+        quickSort(&arr, pi + 1, high); 
     } 
 } 
-void printArray(int arr[], int size) 
+void printArray(int *arr, int size) 
 { 
     int i; 
     for (i=0; i < size; i++) 
@@ -45,8 +45,8 @@ int main()
 { 
     int arr[] = {10, 7, 8, 9, 1, 5}; 
     int n = sizeof(arr)/sizeof(arr[0]); 
-    quickSort(arr, 0, n-1); 
+    quickSort(&arr, 0, n-1); 
     printf("Sorted array: n"); 
-    printArray(arr, n); 
+    printArray(&arr, n); 
     return 0; 
 } 
